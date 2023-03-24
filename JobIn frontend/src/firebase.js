cc91efa6d5b3d3e70b027283067b7022468e8cb1
SayrajKazi <86165595+SayrajKazi@users.noreply.github.com>
2023-03-24 08:30:40 -0500

    Update firebase.js

--- JobIn frontend/src/firebase.js
+++ JobIn frontend/src/firebase.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDqjCCSAbgehiCVzuAjZw7hanLym5RB-v4",
+  apiKey: "",
   authDomain: "auth-jobl.firebaseapp.com",
   projectId: "auth-jobl",
   storageBucket: "auth-jobl.appspot.com",
-  messagingSenderId: "332153639458",
-  appId: "1:332153639458:web:356261a21bcfc5133a307b",
-  measurementId: "G-PPS4EXZVEZ"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
